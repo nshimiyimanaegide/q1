7dc4c88e67ca5abaab9a1d9049e0e1021995744e
nshimiyimanaegide <77850623+nshimiyimanaegide@users.noreply.github.com>
2021-01-25 07:48:38 -0800

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Controller
+{
+	public static void main(String[] args)
+	 {
+
+		FluffyDice FluffyDice;
+		Wheel Wheel;
+		FluffyDice obj1=new FluffyDice("i am a FluffyDice ");
+		Wheel obj2=new Wheel(1,2);
+		Engine obj3=new Engine();
+		Car obj4=new Car();
+	}
+}
