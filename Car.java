74f8b6a80b8272cadf76c00d1af66dc97d9acd3b
nshimiyimanaegide <77850623+nshimiyimanaegide@users.noreply.github.com>
2021-01-25 07:47:54 -0800

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,30 @@
+class Car
+{
+	int count;
+	String name;
+	Engine Engine;
+	Wheel Wheel;
+	FluffyDice FluffyDice;
+	Car(int count,String name,Engine engine,Wheel wheel,FluffyDice FluffyDice)
+	{
+		this.count=count;
+		//this.engine=engine;
+		//this.wheel=wheel;
+		this.FluffyDice=FluffyDice;
+	}
+	String Car()
+	{
+		return null;
+		///Car a=new Car(1,"toyota");
+	}
+void sefluffydice(FluffyDice FluffyDice)
+{
+
+}
+String display()
+{
+	return null;
+}
+
+
+}
