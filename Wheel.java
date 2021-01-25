8c11a3bc3cffbe0a0594a402418eb90b9cdfd520
nshimiyimanaegide <77850623+nshimiyimanaegide@users.noreply.github.com>
2021-01-25 07:50:14 -0800

    Add files via upload

---
+++ Wheel.java
@@ -0,0 +1,20 @@
+class Wheel
+{
+	int count;
+	int position;
+	Wheel(int count,int position)
+	{
+		this.count=count;
+		this.position=position;
+
+	}
+		String display()
+		{
+			System.out.println("am in position 1");
+
+return null;
+
+
+
+		}
+	}
