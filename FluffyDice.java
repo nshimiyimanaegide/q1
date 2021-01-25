1798d85667d015766ead3c8cb049febd8afd545b
nshimiyimanaegide <77850623+nshimiyimanaegide@users.noreply.github.com>
2021-01-25 07:51:04 -0800

    Add files via upload

---
+++ FluffyDice.java
@@ -0,0 +1,20 @@
+/**on my honor,as Rwanda polytechnic/IPRC TUMBA Student,
+i have neither given nor received unauthorized assistance on
+this work.
+@author NSHIMIYIMANA Egide.
+date 25 january 2021.*/
+class FluffyDice
+{
+String color;
+	FluffyDice(String color)
+	{
+		this.color=color;
+	}
+	public String display()
+	{
+		return null;//System.out.println("");
+
+	}
+	
+
+}
